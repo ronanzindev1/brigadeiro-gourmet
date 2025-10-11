16562f095769a087a69b510d5bdf6ec1c52b8288
carita-ucl <caritadm@ucl.br>
2025-10-11 18:55:07 -0300

    Implementa fluxo completo de pedidos: entrada, fila, processamento e atualização de estoque

--- app.py
+++ app.py
@@ -2,10 +2,43 @@ from flask import Flask, Response
 import queue
 import time
 from threading import Thread
-from database.db import popular_banco
+from database.db import *
+from function.envia_pedido_fila import *
+from flask import request, Response
+import json
+
 app = Flask(__name__)
+
 sqs = queue.Queue()
 
+#apaga todos os pedidos
+@app.route('/apaga_pedidos', methods=['GET'])
+def limpa():
+    limpa_tabela_pedidos()
+    return Response(json.dumps({"mensagem": "Tabela apagada com sucesso."}), status=200, mimetype="application/json")
+
+
+# primeira funcao quando chega o pedido é colocar eles na fila processar
+@app.route('/enfileirar_pedidos', methods=['POST'])
+def enfileirar_pedidos():
+    try:       
+       adicionados = envia_pedido_fila(sqs)
+       return Response(json.dumps(adicionados), status=201, mimetype="application/json")
+    except Exception as e:
+        return Response(json.dumps({"error": str(e)}), status=400, mimetype="application/json")
+
+# ver os pedidos
+@app.route('/listar_pedidos', methods=['GET'])
+def listar_pedidos():
+    pedidos = ver_pedidos()
+    return Response(json.dumps(pedidos), status=201, mimetype="application/json")
+
+# ver estoque
+@app.route('/listar_estoque', methods=['GET'])
+def listar_estoque():
+    estoque = ver_estoque()
+    return Response(json.dumps(estoque), status=201, mimetype="application/json")
+    
 def process_order():
     print("Order process initialized")
     while True:
@@ -14,9 +47,7 @@ def process_order():
             print(order)
         else:
             time.sleep(0.5)
-
-
-import functions.criar_pedido
+            
 popular_banco()
 Thread(target=process_order, name="SQS", daemon=True).start()
 app.run(debug=True)
