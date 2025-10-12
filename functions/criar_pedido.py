5295d0dc6080e7422a849b3bb8f907ab5991ecf2
ronanzindev1 <morellianogm@ucl.br>
2025-10-12 16:37:02 -0300

    salvar pedido

--- functions/criar_pedido.py
+++ functions/criar_pedido.py
@@ -1,13 +1,17 @@
 from __main__ import app
 from flask import request, Response
-from database.db import salvar_pedido
+from database.db import salvar_pedido, verifica_estoque
 import json
-@app.route('/order', methods=['POST'])
+from sqs.fila import salva_pedido_fila
+@app.route('/pedido', methods=['POST'])
 def criar_pedido():
     try:
         payload = request.get_json()
+        estoque = verifica_estoque(payload["sabor"])
         pedido = salvar_pedido(payload)
-        # sqs.put(pedido)
+        if estoque is None:
+            return Response(json.dumps({"error": "Sabor não encontrado"}), status=404, headers={"Content-Type": "application/json"})
+        salva_pedido_fila(pedido)
         return Response(json.dumps(pedido), status=201, headers={"Content-Type": "application/json"})
     except Exception as e:
-        return Response(e, status=400)
+        return Response(json.dumps({"error": str(e)}), status=400, headers={"Content-Type": "application/json"})
